a3ecbe82fca03b96fb77f088db75305c3de8f3e2
alejandropedrero <apedrerovega@gmail.com>
2023-04-25 16:50:04 +0200

    test

--- backend/db/database.sql
+++ backend/db/database.sql
@@ -1,21 +1,11 @@
-CREATE DATABASE IF NOT EXISTS tecladb;
+-- CREATE DATABASE IF NOT EXISTS tecladb;
 
-USE tecladb;
+-- USE tecladb;
 
-CREATE TABLE user (
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    name VARCHAR(45) NOT NULL,
-    username VARCHAR(15) NOT NULL, 
-    PRIMARY KEY(id)
-);
+-- CREATE TABLE user (
+--     id INT(11) NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(45) NOT NULL,
+--     username VARCHAR(15) NOT NULL, 
+--     PRIMARY KEY(id)
+-- );
 
-INSERT INTO usuarios (id_usuario, nombre_completo, nombre_usuario, correo_electronico, fecha_nacimiento, ubicacion, genero, intereses) 
-VALUES (1, 'Juan Perez', 'jperez', 'jperez@example.com', '1990-01-01', 'Ciudad de Mexico', 'Masculino', 'Deportes,
-
-INSERT INTO usuarios (id_usuario, nombre_completo, nombre_usuario, correo_electronico, fecha_nacimiento, ubicacion, genero, intereses) 
-VALUES (1, 'Juan Perez', 'jperez', 'jperez@example.com', '1990-01-01', 'Ciudad de Mexico', 'Masculino', 'Deportes,
-
-INSERT INTO usuarios (id_usuario, nombre_completo, nombre_usuario, correo_electronico, fecha_nacimiento, ubicacion, genero, intereses) 
-VALUES (1, 'Juan Perez', 'jperez', 'jperez@example.com', '1990-01-01', 'Ciudad de Mexico', 'Masculino', 'Deportes,
-
-DESCRIBE user;
