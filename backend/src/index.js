e59c3d1f027b1183df5a03c61cb49f37f51b6a64
alejandropedrero <apedrerovega@gmail.com>
2023-04-26 01:11:02 +0200

    intentando login

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,4 +11,4 @@ app.use(indexRoutes);
 
 app.listen(3000);
 
-console.log("Server running on port 3000");
+console.log(`Server running on port ${3000}`);
--- backend/src/routes/login.routes.js
+++ backend/src/routes/login.routes.js
@@ -4,5 +4,6 @@ import { getLogin } from "../controllers/login.controller.js";
 const router = Router();
 
 router.get("/login", getLogin);
+router.post("/login");
 
 export default router;
