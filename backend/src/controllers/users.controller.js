05f4d08081d299e18374722c2f23a6abb1679645
alejandropedrero <apedrerovega@gmail.com>
2023-04-28 13:02:32 +0200

    rama con login registro y pagina main para la demo del viernes 28

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -4,9 +4,3 @@ export const getUsers = async (req, res) => {
   const [rows] = await pool.query("SELECT id, name, email FROM users");
   res.json(rows);
 };
-
-export const createUser = (req, res) => res.send("creando usuarios");
-
-export const updateUser = (req, res) => res.send("actualizando usuarios");
-
-export const deleteUser = (req, res) => res.send("eliminando usuarios");
